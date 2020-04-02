e5957ec6516920de4970250eff3c69c3e1666d90
2862297064 <15035990976.@163.com>
2020-04-02 10:15:31 +0800

    静态文件

--- day1/watchlist/app.py
+++ day1/watchlist/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, url_for, render_template
 
 app = Flask(__name__)
 
@@ -10,8 +10,18 @@ app = Flask(__name__)
 #     return "hello word david111!"
 
 
-@app.route("/user/<name>")
-def index(name):
-    return "hello %s"%name
+@app.route("/user/")
+def index():
+    name = "David"
+    movies = [
+        {"title": "大赢家", "year": "2020"},
+        {"title": "囧妈", "year": "2020"},
+        {"title": "叶问", "year": "2020"},
+        {"title": "疯狂外星人", "year": "2019"},
+        {"title": "大话西游", "year": "1999"},
+        {"title": "月光宝盒", "year": "1996"},
+        {"title": "速度与激情8", "year": "2020"}
+    ]
+    return render_template("index.html", name=name, movies=movies)
 
 
