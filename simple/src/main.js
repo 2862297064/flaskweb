62e12e81a740d0143fe75f406b45638b5941e46c
2862297064 <15035990976.@163.com>
2020-04-16 08:37:25 +0800

    实战布局&首页

---
+++ simple/src/main.js
@@ -0,0 +1,23 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import IndexPage from './pages/index.vue'
+import Layout from './Layout.vue'
+
+Vue.use(VueRouter)
+let router = new VueRouter({
+  mode:'history',
+  routes: [
+    { path:'/',component:IndexPage },
+  ]
+})
+
+
+new Vue({
+  el: '#app',
+  router,
+  // render: h => h(App),
+  components:{
+    Layout
+  },
+  template:'<Layout/>'
+})
