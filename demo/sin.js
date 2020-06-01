635ed51ed1cb8fa4be6d2632e41d95c74ae99fc4
david <15035990976@163.com>
2020-06-01 09:10:57 +0800

    '确认无误，最后一次提交'

---
+++ demo/sin.js
@@ -0,0 +1,5 @@
+var result = Math.tan(Math.PI* 30 / 180);
+console.log('sin30--->', result);
+
+
+
--- vuePlugin/script.js
+++ vuePlugin/script.js
@@ -1,13 +1,22 @@
 import Vue from 'vue/dist/vue.js'
 import VueMathPlugin from './VueMathPlugin.js'
+import Vuex from "vuex"
 
 Vue.use(VueMathPlugin)
+Vue.use(Vuex)
 
 new Vue({
     el:'#app',
     data:{
         item:30
-    }
+    },
+    store:Store
 })
 
+var store = new Vuex.store({
+    state:{message: 'Hello'},
+    mutations:{},
+    actions:{},
+    getters:{}
+})
 
